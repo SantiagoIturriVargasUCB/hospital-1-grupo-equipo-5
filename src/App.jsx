5e721109ca2c69354496287e2eef88f666598d44
SantiagoIturriVargasUCB <santiago.iturri@ucb.edu.bo>
2024-03-07 11:21:47 -0400

    First commite, yarn vite react app created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>Este es el proyecto de Hospital 1 del Grupo 5 de Sistemas de Informacion I</div>
+    </>
+  )
+}
+
+export default App
